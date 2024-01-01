1c50070dcd03cea4ba1fc2549208465ecd6e15b4
bigballahransom <105400568+bigballahransom@users.noreply.github.com>
2024-01-01 14:45:12 -0800

    x

--- components/Coming.js
+++ components/Coming.js
@@ -5,7 +5,7 @@ const Coming = () => {
     <div clasName='flex p-8 bg-white'>
 <div>
     <h1 className='font-bold text-3xl text-orange text-center'>Check Back Soon</h1>
-    <p className='text-primary text-center pt-2'>Check back January 1st, 2024 for more info regarding hotels, attire, menu, etc...</p>
+    <p className='text-primary text-center pt-2'>Check back Feb. 1st, 2024 for more info regarding hotels, attire, menu, etc...</p>
 </div>
 </div>
   )
