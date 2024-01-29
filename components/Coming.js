6a832ee17bee5726dedac280aaafcca641c2e361
bigballahransom <105400568+bigballahransom@users.noreply.github.com>
2024-01-28 18:10:03 -0800

    food

--- components/Coming.js
+++ components/Coming.js
@@ -4,8 +4,9 @@ const Coming = () => {
   return (
     <div clasName='flex p-8 bg-white'>
 <div>
-    <h1 className='font-bold text-3xl text-orange text-center'>Check Back Soon</h1>
-    <p className='text-primary text-center pt-2'>Check back Feb. 1st, 2024 for more info regarding hotels, attire, menu, etc...</p>
+    <h1 className='font-bold text-3xl text-orange text-center'>Check Back For Menu</h1>
+    <p className='text-primary text-center pt-2'>For now please alert us if you have any allergies!</p>
+    <button className='flex gap-1 bg-orange text-primary px-4 py-2 rounded-full hover:bg-backdrop items-center justify-center' href='https://form.jotform.com/240255476365157'>Food Allergies</button>
 </div>
 </div>
   )
