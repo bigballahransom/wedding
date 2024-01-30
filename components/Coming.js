692bb06397c116277b419ef7392434dc5fcc62b4
bigballahransom <105400568+bigballahransom@users.noreply.github.com>
2024-01-29 17:13:22 -0800

    fixed menu button

--- components/Coming.js
+++ components/Coming.js
@@ -1,12 +1,15 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Coming = () => {
   return (
-    <div clasName='flex p-8 bg-white'>
+    <div clasName='flex p-8 bg-white justify-center'>
 <div>
     <h1 className='font-bold text-3xl text-orange text-center'>Check Back For Menu</h1>
     <p className='text-primary text-center pt-2'>For now please alert us if you have any allergies!</p>
-    <button className='flex gap-1 bg-orange text-primary px-4 py-2 rounded-full hover:bg-backdrop items-center justify-center' href='https://form.jotform.com/240255476365157'>Food Allergies</button>
+    <Link className='flex justify-center items-center mt-8' href='https://form.jotform.com/240255476365157'>
+    <button className='flex gap-1 bg-orange text-primary px-4 py-2 rounded-full hover:bg-backdrop items-center justify-center'>Food Allergies</button>
+    </Link>
 </div>
 </div>
   )
